51071c11356615bf6806f0ae5eab2a653c1463d5
Joshardals <joshuabamidele219@gmail.com>
2024-01-23 16:21:54 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
 export default function Home() {
   return (
-    <div className="bg-red-500 h-56 grid grid-cols-2 gap-4 place-content-between">
-      <div>01</div>
-      <div>02</div>
-      <div>03</div>
-      <div>04</div>
-    </div>
+    <main>
+      <div className="element"></div>
+    </main>
   );
 }
