aee9d267c15da089f2490ac33fb49c07de5f8ed8
Joshardals <joshuabamidele219@gmail.com>
2024-01-23 13:43:02 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
-
 export default function Home() {
-  return <main>Hello World!</main>;
+  return (
+    <main className="flex flex-wrap-reverse *:bg-blue-500">
+      <div className="flex-1">1</div>
+      <div className="flex-1">2</div>
+      <div>3</div>
+    </main>
+  );
 }
