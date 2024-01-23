03eb63448f85e9ba6bec79303da905a37262d4e4
Joshardals <joshuabamidele219@gmail.com>
2024-01-23 18:12:15 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main>
+    <main className="flex items-center justify-center min-h-screen">
       <div className="element">
         <p>Hello World!</p>
       </div>
