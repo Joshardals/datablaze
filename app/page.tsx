7d458f87582d92b7aa411fb645e31a23f195e2e6
Joshardals <joshuabamidele219@gmail.com>
2024-01-23 21:12:40 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
+"use client";
+
+import { useRef } from "react";
+
 export default function Home() {
+  const ref = useRef(0);
+  const handleClick = () => {
+    ref.current = ref.current + 1;
+    alert(`You clicked the button ${ref.current} times`);
+  };
   return (
     <main className="flex items-center justify-center min-h-screen">
       <div className="element">
         <p>Hello World!</p>
       </div>
-      <button className="bg-red-500 px-4 py-1">Click Me!</button>
+      <button onClick={handleClick} className="bg-red-500 px-4 py-1">
+        Click Me!
+      </button>
     </main>
   );
 }
