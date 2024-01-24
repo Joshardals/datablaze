300695f5a502a7ec9fd15effbc0b56fe876f4d8d
Joshardals <joshuabamidele219@gmail.com>
2024-01-24 13:13:55 +0100

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
-"use client";
-
-import { useRef } from "react";
-
 export default function Home() {
-  const ref = useRef("I am pointing!");
   return (
     <main className="flex items-center justify-center min-h-screen">
-      <div className="element">
-        <p>Hello World!</p>
-        <p>{ref.current}</p>
-      </div>
+      <button className="bg-teal-700 text-black font-medium py-2 mx-4 rounded-md">
+        Go to dashboard
+      </button>
     </main>
   );
 }
