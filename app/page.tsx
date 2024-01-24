5e1ca7ed848f4a63d70b04114507a7b7663c8f5a
Joshardals <joshuabamidele219@gmail.com>
2024-01-24 14:30:44 +0100

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex items-center justify-center min-h-screen">
       <Link
-        className="bg-teal-500 text-black font-medium py-2 px-4 rounded-md"
+        className=" bg-teal-500 text-black font-medium py-2 px-4 rounded-md"
         href="/dashboard"
       >
         Go to dashboard
