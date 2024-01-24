7b86708d8263fcc2e0208abfde00c4935ef0ace0
Joshardals <joshuabamidele219@gmail.com>
2024-01-24 15:54:04 +0100

    initial commit

--- app/api/items/route.ts
+++ app/api/items/route.ts
@@ -1,4 +1,7 @@
 import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export async function GET(request: Request) {
   return NextResponse.json(
