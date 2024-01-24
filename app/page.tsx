c77be5fd874dbdc8c6240f325b8a7fa0b3bfbe7c
Joshardals <joshuabamidele219@gmail.com>
2024-01-24 16:34:06 +0100

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   const posts = await fetchPosts();
   console.log(posts);
   return (
-    <main className="flex items-center justify-center min-h-screen">
+    <main className="flex flex-col space-y-2 items-center justify-center min-h-screen">
       <Link
         className=" bg-teal-500 text-black font-medium py-2 px-4 rounded-md"
         href="/dashboard"
@@ -13,7 +13,14 @@ export default async function Home() {
         Go to dashboard
       </Link>
 
-      
+      {posts?.map((post) => {
+        const { id, title } = post;
+        return (
+          <p key={id} className=" text-gray-500">
+            {title}
+          </p>
+        );
+      })}
     </main>
   );
 }
