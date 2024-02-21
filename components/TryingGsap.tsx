502d370a130da09fb8db4dff502262c60c01c360
Joshardals <joshuabamidele219@gmail.com>
2024-02-21 15:44:26 +0100

    initial commit

--- components/TryingGsap.tsx
+++ components/TryingGsap.tsx
@@ -1,32 +1,32 @@
-// "use client";
-// import { useRef } from "react";
-// import gsap from "gsap";
-// import { useGSAP } from "@gsap/react";
-// import { ScrollTrigger } from "gsap/ScrollTrigger";
+"use client";
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-// export default function TryingGsap() {
-//   const container = useRef<HTMLDivElement | null>(null);
+export default function TryingGsap() {
+  const container = useRef<HTMLDivElement | null>(null);
 
-//   useGSAP(
-//     () => {
-//       gsap.registerPlugin(ScrollTrigger);
+  useGSAP(
+    () => {
+      gsap.registerPlugin(ScrollTrigger);
 
-//       gsap.to(".c", {
-//         scrollTrigger: ".c",
-//         x: 400,
-//         rotate: 360,
-//         duration: 3,
-//       });
-//     },
-//     { scope: container }
-//   );
+      gsap.to(".c", {
+        scrollTrigger: ".c",
+        x: 400,
+        rotate: 360,
+        duration: 3,
+      });
+    },
+    { scope: container }
+  );
 
-//   return (
-//     // <div className="bg-white w-full h-[80px] fixed top-0 header">Header</div>
-//     <div className="p-5 space-y-56" ref={container}>
-//       <div className="a p-10 bg-red-500 w-24 cursor-pointer">A</div>
-//       <div className="b p-10 bg-blue-500 w-24 cursor-pointer">B</div>
-//       <div className="c p-10 bg-orange-500 w-24 cursor-pointer">C</div>
-//     </div>
-//   );
-// }
+  return (
+    // <div className="bg-white w-full h-[80px] fixed top-0 header">Header</div>
+    <div className="p-5 space-y-56" ref={container}>
+      <div className="a p-10 bg-red-500 w-24 cursor-pointer">A</div>
+      <div className="b p-10 bg-blue-500 w-24 cursor-pointer">B</div>
+      <div className="c p-10 bg-orange-500 w-24 cursor-pointer">C</div>
+    </div>
+  );
+}
