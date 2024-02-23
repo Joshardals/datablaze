d2b24bf36698f91f6317e167bde4e2a5fe7dfd8c
Joshardals <joshuabamidele219@gmail.com>
2024-02-23 14:13:43 +0100

    initial commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,10 +5,30 @@ import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default function Header() {
-  const container = useRef<HTMLDivElement | null>(null);
+  useGSAP(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const showAnim = gsap
+      .from(".header", {
+        yPercent: -100,
+        paused: true,
+        duration: 0.2,
+      })
+      .progress(1);
+
+    ScrollTrigger.create({
+      start: "top top",
+      end: "max",
+      onUpdate: (self) => {
+        self.direction === -1 ? showAnim.play() : showAnim.reverse();
+      },
+    });
+  });
   return (
-    <div ref={container}>
-      <div className="bg-white px-6 py-4">Moving Header On Scroll</div>
+    <div>
+      <div className="bg-white px-6 py-4 header fixed top-0 left-0 w-full flex items-center justify-center">
+        Header
+      </div>
     </div>
   );
 }
--- components/TryingGsap.tsx
+++ components/TryingGsap.tsx
@@ -7,28 +7,28 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 export default function TryingGsap() {
   const container = useRef<HTMLDivElement | null>(null);
 
-  useGSAP(
-    () => {
-      gsap.registerPlugin(ScrollTrigger);
+  // useGSAP(
+  //   () => {
+  //     gsap.registerPlugin(ScrollTrigger);
 
-      gsap.to(".c", {
-        scrollTrigger: {
-          trigger: ".c",
+  //     gsap.to(".c", {
+  //       scrollTrigger: {
+  //         trigger: ".c",
 
-          markers: true,
-          scrub: true,
-        },
-        x: 400,
-        rotate: 360,
-        duration: 3,
-      });
-    },
-    { scope: container }
-  );
+  //         markers: true,
+  //         scrub: true,
+  //       },
+  //       x: 400,
+  //       rotate: 360,
+  //       duration: 3,
+  //     });
+  //   },
+  //   { scope: container }
+  // );
 
   return (
     // <div className="bg-white w-full h-[80px] fixed top-0 header">Header</div>
-    <div className="p-5 space-y-56" ref={container}>
+    <div className="p-5 space-y-56 container" ref={container}>
       <div className="a p-10 bg-red-500 w-24 cursor-pointer">A</div>
       <div className="b p-10 bg-blue-500 w-24 cursor-pointer">B</div>
       <div className="c p-10 bg-orange-500 w-24 cursor-pointer">C</div>
