b22b56170a98c0717ae67cda66cba135feaf6e74
Joshardals <joshuabamidele219@gmail.com>
2024-02-25 17:57:01 +0100

    initial commit

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <div className="uppercase flex items-center justify-center min-h-screen text-4xl text-white">
+      about page
+    </div>
+  );
+}
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,10 @@ import { useRef } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Link from "next/link";
 
 export default function Header() {
+  const header = useRef<HTMLDivElement | null>(null);
   useGSAP(() => {
     gsap.registerPlugin(ScrollTrigger);
 
@@ -26,8 +28,23 @@ export default function Header() {
   });
   return (
     <div>
-      <div className="bg-white px-6 py-4 header fixed top-0 left-0 w-full flex items-center justify-center">
-        Header
+      <div
+        className=" bg-white text-black px-6 py-4 header fixed top-0 left-0 w-full flex items-center justify-center"
+        ref={header}
+      >
+        <nav>
+          <ul className="flex items-center justify-center space-x-14">
+            <li className="active">
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="contact.html">Contact</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
